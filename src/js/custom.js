ec62138d6539049810120941e660d4ee55ab2ad9
Nikita Kolotilo <nikita.kolotilo@gmail.com>
2019-05-14 15:57:28 +0300

    Eslint; anchors; custom js on scrolling; fonts; image sprites

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,16 +1,29 @@
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.getElementById("myBtn").style.display = "block";
   } else {
     document.getElementById("myBtn").style.display = "none";
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+function up() {
+  const top = Math.max(document.body.scrollTop, document.documentElement.scrollTop);
+  if (top > 0) {
+    window.scrollBy(0, ((top + 100) / -10));
+    let t = setTimeout('up()', 20);
+  } else clearTimeout(t);
+  return false;
 }
+
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
