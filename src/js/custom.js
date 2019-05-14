f37ef2d0389ad0833dcd1c9cd8bf748473fb6c9e
Nikita Kolotilo <nikita.kolotilo@gmail.com>
2019-05-14 18:33:31 +0300

    Portfolio grayscale hover effect

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,29 +1,52 @@
-window.onscroll = function () { scrollFunction() };
-
+/* eslint-disable no-unused-vars */
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("myBtn").style.display = "block";
+    document.getElementById('myBtn').style.display = 'block';
   } else {
-    document.getElementById("myBtn").style.display = "none";
+    document.getElementById('myBtn').style.display = 'none';
   }
 }
 
+window.onscroll = () => scrollFunction();
+
 function up() {
   const top = Math.max(document.body.scrollTop, document.documentElement.scrollTop);
+  let t;
   if (top > 0) {
     window.scrollBy(0, ((top + 100) / -10));
-    let t = setTimeout('up()', 20);
+    // eslint-disable-next-line no-implied-eval
+    t = setTimeout('up()', 20);
   } else clearTimeout(t);
   return false;
 }
 
 // Smooth scrolling
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener('click', function scroll(e) {
     e.preventDefault();
 
     document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
+      behavior: 'smooth',
     });
   });
 });
+
+// grayscale off on hover
+const grayscalesList = document.getElementsByClassName('grayscale');
+[].forEach.call(grayscalesList, (el) => {
+  el.addEventListener('mouseover', () => {
+    el.classList.remove('grayscale');
+  });
+
+  el.addEventListener('mouseout', () => {
+    el.classList.add('grayscale');
+  });
+});
+
+
+// grayscales.forEach(element => {
+
+// });
+// grayscales.addEventListener('hover', function cancelGrayscale(event) {
+
+// });
